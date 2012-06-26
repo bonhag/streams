e0e2b8b4338a4c69e6f5a7bd8f727f171b207f9c
Jeff Bonhag <bonhagjeffrey@gmail.com>
2012-06-26 12:16:54 -0400

    a more helpful client

--- client/client.rb
+++ client/client.rb
@@ -1,6 +1,15 @@
 require 'rest_client'
 
-response = RestClient.put 'http://localhost:4567/update/office',
+host = "growing-window-4124.herokuapp.com"
+
+puts "Please enter the name of your source:"
+source = gets.chomp
+source_sanitized = source.gsub(/[^A-Za-z]/, '')
+source = source_sanitized
+
+puts "You can watch this stream at http://#{host}/watch/#{source}"
+
+response = RestClient.put "http://#{host}/update/#{source}",
                             :file => File.new("capture.jpg", 'rb')
 
 puts response.code
