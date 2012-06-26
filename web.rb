740df236015f92b958ea4e3adbe4085f47b3a681
Jeff Bonhag <bonhagjeffrey@gmail.com>
2012-06-26 10:08:52 -0400

    Changed client to Ruby, staying away from databases

--- web.rb
+++ web.rb
@@ -1,7 +1,31 @@
 require 'sinatra'
+require 'sequel'
+
+# Sequel.connect(ENV['DATABASE_URL'])
+
+sources = {}
 
 get '/' do
   erb :index
+end
+
+get '/watch/:source' do
+  content_type 'image/jpeg'
+  source = params[:source]
+  open(sources[source], 'rb').read()
+end
+
+post '/create/:source' do
+end
+
+put '/update/:source' do
+  source = params[:source]
+  the_file_object = params[:file][:tempfile] # this is the file object itself.
+  sources[source] = File.absolute_path(the_file_object)
+end
+
+get '/dbtest' do
+  DB = Sequel.connect(ENV['DATABASE_URL'])
 end
 
 post '/data' do
