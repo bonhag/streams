2f9d8578bd6ce87b118cfa3627643b686413c658
Jeff Ree <bonhag@localhost>
2012-12-09 00:48:25 -0500

    tests a crashin

--- streams.rb
+++ streams.rb
@@ -3,35 +3,15 @@ require 'kramdown'
 require 'json'
 require 'haml'
 
-set :all_streams, {}
+set :streams, {}
 
-helpers do
+def save_stream id, data
+  settings.streams[id] = {:data => data, :version => Time.now.to_i}
+  settings.streams[id][:version]
+end
 
-  def protected!(stream)
-    unless authorized?(stream)
-      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
-      throw(:halt, [401, "Not authorized\n"])
-    end
-  end
-
-  def authorized?(stream)
-    @auth ||= Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [stream, stream]
-  end
-
-  def generate_password(length=16)
-    chars = [*?a..?z, *?A..?Z, *0..9]
-    (1..length).map{ chars.sample }.join
-  end
-
-  def stream_reserved?(stream)
-    # a stream exists if there is a password set for it
-    false
-  end
-
-  def new_version? stream_name, current_version
-    'no'
-  end
+def load_stream id
+  settings.streams[id]
 end
 
 #ENV['RACK_ENV'] = "production"
@@ -41,59 +21,33 @@ end
 
 get '/streams/:id' do
   content_type 'image/jpeg'
-  settings.all_streams[params[:id]]
+  stream = load_stream params[:id]
+  stream.data
 end
 
 put '/streams/:id' do
   content_type :json
-  #settings.all_streams[params[:id]] = request.body.read
+  version = save_stream params[:id], request.body.read
+
   # version (time travel)
-  {:last_updated => Time.now.to_i}.to_json
+  {:version => version}.to_json
 end
 
 get '/watch/:id' do
-  @name = params[:id]
-  @version = Time.new.to_i
   haml :watch
   end
   
-get '/create/:stream' do
-  nil
-end
- 
-post '/is' do
+post '/stream_should_update' do
   content_type :json
-  {:answer => 'no'}.to_json
-end
+  my_version = params[:version]
 
-get '/check' do
-  content_type :json
-  # get the name of the stream we're watching
-  begin
-    what_am_i_watching = request.referrer.split('/')[-1]
-  rescue NoMethodError
-    puts 'Somebody tried to do something bad'
+  id = request.referrer.split('/')[-1]
+  stream = load_stream id
+  answer = 'no'
+  if my_version < stream[:version]
+    answer = 'yes'
   end
-
-
-  # check to see whether there's a new version of that stream
-  json_request = JSON.parse request.body.to_s
-  current_version = json_request.version
-  answer = new_version? what_am_i_watching, current_version
   {:answer => answer}.to_json
-end
-
-# Note, these two are duplicates, and, I guess, deprecated.
-put '/update/:stream' do
-  settings.all_streams[params[:stream]] = request.body.read
-end
-
-put '/upload/:id' do
-  settings.all_streams[params[:id]] = request.body.read
-end
-
-delete '/delete/:stream' do
-  nil
 end
 
 get '/' do
@@ -110,7 +64,6 @@ end
 get '/streams.json' do
   streams.to_json
 end
-
 
 get '/client/' do
   # instructions for client
--- test/test_streams.rb
+++ test/test_streams.rb
@@ -9,14 +9,6 @@ class StreamsTest < Test::Unit::TestCase
     Sinatra::Application
   end
 
-  # pollute
-  def test_no_streams_exist
-    post '/is', {:version => Time.now.to_i}
-    #, 'HTTP_REFERER' => 'http://localhost:4567/watch/shabazz'
-    json_response = JSON.parse last_response.body
-    assert_equal json_response['answer'], 'no'
-  end
-
   def test_updating_a_stream
     start_time = Time.now.to_i
 
@@ -28,18 +20,46 @@ class StreamsTest < Test::Unit::TestCase
 
     # take a number
     json_response = JSON.parse last_response.body
-    last_updated = json_response['last_updated']
+    version = json_response['version']
 
-    assert last_updated >= start_time, "#{last_updated} should be later than #{start_time}"
+    assert version >= start_time, "#{version} should be later than #{start_time}"
   end
 
-  def not_yet
-    post '/is', {:stream => 'radiohead', :version => version.to_i - 1}
-    json_response = JSON.parse last_response_body
+  # pollute
+  def test_should_update_if_no_streams_exist
+    post '/stream_should_update', {}, 'HTTP_REFERER' =>
+      'http://localhost:4567/watch/coming_to_america'
 
-    assert_equal json_response['answer'], 'yes'
+    json_response = JSON.parse last_response.body
+    p json_response
+    assert_equal 'no', json_response['answer']
+  end
+
+  def _test_my_version_is_earlier_than_latest_version
+    put '/streams/shabazz.jpg', 'black up'
+
+    post '/stream_should_update', {:my_version => 0},
+      'HTTP_REFERER' => 'http://localhost:4567/watch/shabazz'
+
+    json_response = JSON.parse last_response.body
+    assert_equal 'yes', json_response['answer']
+  end
+
+  def _test_data_returned_if_new_version_available
+    put '/streams/radiohead.jpg', 'in rainbows'
+
+    post '/stream_should_be_updated', {:my_version => 0},
+      'HTTP_REFERER' => 'http://localhost:4567/watch/radiohead'
+
+    json_response = JSON.parse last_response.body
     assert_equal json_response['and_here_it_is'], 'in rainbows'
   end
 
+  def test_save_stream_data
+    save_stream 'big cat', 'soft and furry'
+    stream = load_stream 'big cat'
+    assert_equal 'soft and furry', stream[:data]
+  end
+    
     
 end
