9a7693f054fb9269d9890dbc792fc2d6ea0e24f8
Jeff Bonhag <bonhag@localhost>
2013-03-07 23:29:59 -0500

    Revert to version 1 of the API

--- clients/anti_select_timeout.py
+++ clients/anti_select_timeout.py
@@ -1,8 +1,16 @@
 import cv
-import requests
+import os
+
+
+stream = 'raspberrypitesting'
 
 camcapture = cv.CreateCameraCapture(0)
 cv.SetCaptureProperty(camcapture,cv.CV_CAP_PROP_FRAME_WIDTH, 320)
 cv.SetCaptureProperty(camcapture,cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
-frame = cv.QueryFrame(camcapture)
-cv.SaveImage("output.jpg", frame)
+
+while True:
+	frame = cv.QueryFrame(camcapture)
+	cv.SaveImage("%s.jpg" % stream, frame)
+
+	# Post to stream
+	os.system("curl -T %s.jpg http://www.pauljohninternetart.info/update/%s" % (stream, stream))
